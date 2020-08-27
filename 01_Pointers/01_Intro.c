c512ac887d5aeeccd45d128eed4b85755e13d755
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-27 19:46:50 +0530

    Update 01_Intro.c

--- 01_Pointers/01_Intro.c
+++ 01_Pointers/01_Intro.c
@@ -7,15 +7,17 @@ int main()
 	int *p;
 	p = &a;
 	printf("The value of a = %d", a);
-	printf("The value of p = %d", p);
-	printf("The address of a = %d", &a);
-	printf("The value at p is %d", *p); // Derefrencing
+	printf("\nThe value of p = %d", p);
+	printf("\nThe address of a = %d", &a);
+	printf("\nThe value at p is %d", *p); // Derefrencing
 	int **p1; // Pointer to pointer
 	p1 = &p;
-	printf("The value of p1 %d",p1);
-	printf("The address of p1 %d",&p1);
-	printf("The value of address at *p1 %d", *p1);
-	printf("The value of address at **p1 %d", **p1);
+	printf("\nThe value of p1 %d",p1);
+	printf("\nThe address of p1 %d",&p1);
+	printf("\nThe value of address at *p1 %d", *p1);
+	printf("\nThe value of address at **p1 %d", **p1);
+	**p1 = 5;
+	printf("\nModified value of a from **p1 %d", a);
 
 	return 0;
 }
