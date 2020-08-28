144f7336d4bf5af963744e40729e57a6e9268a4d
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-28 20:53:25 +0530

    Update 03_Argument.c

--- 01_Pointers/03_Argument.c
+++ 01_Pointers/03_Argument.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+void fun(int *a);
+
+void main()
+{
+	int a = 10;
+	fun(&a); // Call by refrence
+	printf(" a = %d", a);
+}
+
+void fun(int *a) // Pointer argument
+{
+	*a = *a * *a;
+}
