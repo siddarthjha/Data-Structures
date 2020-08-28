5d654d139ee73f2b44f2b7e7d47a209b1e66266b
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-28 21:17:57 +0530

    Update 03_Argument.c

--- 01_Pointers/03_Argument.c
+++ 01_Pointers/03_Argument.c
@@ -7,9 +7,26 @@ void main()
 	int a = 10;
 	fun(&a); // Call by refrence
 	printf(" a = %d", a);
+	int a[5] = {1, 2, 3, 4, 5};
+	int size;
+	size = sizeof(a)/ sizeof(a[0]);
+	size = arr(a, size);
+	printf("sum = %d", size);
 }
 
 void fun(int *a) // Pointer argument
 {
 	*a = *a * *a;
+}
+
+int arr(int b[], int size)
+{
+	int sum = 0;
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		sum += b[i];
+		printf("sum = %d", sum);
+	}
+	return sum;
 }
