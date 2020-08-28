adadee0409ab34cee16a780a0eb629a6229dd4bb
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-28 20:10:46 +0530

    Update 02_Types.c

--- 01_Pointers/02_Types.c
+++ 01_Pointers/02_Types.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+void main()
+{
+	int x = 1032;
+	int *p;
+	p = &x;
+	char *c;
+	c = (char*)p;
+	// Char pointer has 1 bytes size and int has 4 bytes size so the value may be defered
+	printf("*p = %d, *c = %d",*p, *c);
+	void *v;
+	v = p;
+	printf(" v = %d", v); // But we cannot derefrence void pointer like other data type pointers
+	
+}
