20e6545842abfb5cae44a455adf3b3c24546b0fa
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-29 21:42:53 +0530

    Update 04_Array.c

--- 01_Pointers/04_Array.c
+++ 01_Pointers/04_Array.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main()
+{
+	char *c = "Siddarth";
+	printf("%s", c);
+	int ar[2][3] = {{1,2,3},{4,5,6}};
+	int (*p)[3] = ar;
+	printf("\n%d", *p+1);
+	int arr[3][2][2] = { 
+	    				{{1,2},  {3,4} },
+						{{5,6},  {7,8} },
+						{{9,10}, {11,12} }
+						};
+	int (*p1) [2][2] = arr;
+	printf("\n%d", *p1);
+	return 0;
+}
