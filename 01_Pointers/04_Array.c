08853be2f50e613d580e9020b5aad0d5e893ab39
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-29 21:47:58 +0530

    Update 04_Array.c

--- 01_Pointers/04_Array.c
+++ 01_Pointers/04_Array.c
@@ -5,13 +5,25 @@ int main()
 	printf("%s", c);
 	int ar[2][3] = {{1,2,3},{4,5,6}};
 	int (*p)[3] = ar;
+	f(ar);
 	printf("\n%d", *p+1);
 	int arr[3][2][2] = { 
-	    				{{1,2},  {3,4} },
-						{{5,6},  {7,8} },
-						{{9,10}, {11,12} }
-						};
+	    			{{1,2},  {3,4} },
+				    {{5,6},  {7,8} },
+					{{9,10}, {11,12} }
+			     };
 	int (*p1) [2][2] = arr;
 	printf("\n%d", *p1);
+	fun(arr);
 	return 0;
+}
+
+void f((*ar)[3])
+{
+// Same goes to this
+}
+
+void fun((*arr)[2][2])
+{
+	// Rather than creating a new array we can use pointer to avoid wastage of memory
 }
