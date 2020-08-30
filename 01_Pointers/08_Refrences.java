487cc06585b53fe556796ea95476946f2d95f63a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-30 19:35:53 +0530

    Update 08_Refrences.java

--- 01_Pointers/08_Refrences.java
+++ 01_Pointers/08_Refrences.java
@@ -1,1 +1,17 @@
-
+class A
+{
+	void fun()
+	{
+		System.out.println("Function");
+	}
+}
+public class Refrences
+{
+	public static void main(String[] args)
+	{
+    // Any time you are creating an object in java you are creating a pointer to object
+		A obj = new A();
+		// obj is a reference of A containg it's address of A's object
+		obj.fun();
+	}
+}
