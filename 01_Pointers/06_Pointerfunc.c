f1247934e615cd8d172c59c093e0a408742e2858
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-30 17:21:32 +0530

    Update 06_Pointerfunc.c

--- 01_Pointers/06_Pointerfunc.c
+++ 01_Pointers/06_Pointerfunc.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int *square(int *x, int *y);
+void main()
+{
+	int x , y, *z;
+	printf("Enter two numbers for multiplication\n");
+	scanf("%d %d", &x, &y);
+	z = square(&x, &y); // If we call any other function here then the value of local variable flushes out 
+	printf("value = %d", *z);
+	
+}
+
+int *square(int *x, int *y)
+{
+	int *z;
+	z = (int*)malloc(sizeof(int));// So instead of a local variable we have allocated it's memory in heap
+	*z = (*x )* (*y);
+	return z;
+}
 
