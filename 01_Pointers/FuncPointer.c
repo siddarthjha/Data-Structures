e4bfe4715b909c98c3ab21fe3725ef3e84c769a1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-30 17:38:41 +0530

    Add files via upload

---
+++ 01_Pointers/FuncPointer.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int square(int x, int y);
+void main()
+{
+	int x , y, (*z)(int, int);
+	printf("Enter two numbers for multiplication\n");
+	scanf("%d %d", &x, &y);
+	z = square;
+	int v;
+	v = (*z)(x, y);
+	printf("value = %d", v);
+	
+	
+}
+
+int square(int x, int y)
+{
+	return x*y;
+}
+
