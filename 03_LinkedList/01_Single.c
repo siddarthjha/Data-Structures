1bbf85b75906bf894ddbf1a2b08dd62a4fa57f42
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-31 21:50:26 +0530

    Update 01_Single.c

--- 03_LinkedList/01_Single.c
+++ 03_LinkedList/01_Single.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct abc
+{
+	int data;
+	struct abc *next;
+};
 
+int main()
+{
+	struct abc *head;
+	Printf("Enter the choice\n1.Insert the value\n2.Print the value");
+	int i, value;
+	scanf("%d", &i);
+	printf("\nEnter value");
+	scanf("%d", &value);
+	if(i == 1)
+		insert(value);
+	if(i ==2)
+		print();
+	
+	return 0;
+}
+
+void insert(int value)
+{
+	struct abc *ptr;
+	ptr = (struct abc*)malloc(sizeof(struct abc));
+	ptr.data = value;
+}
+
+void print()
+{
+	
+}
