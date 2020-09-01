35a244058b6072dd3e8e5251e1475b1a30f2350c
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-01 21:29:23 +0530

    Update 01_Single.c

--- 03_LinkedList/01_Single.c
+++ 03_LinkedList/01_Single.c
@@ -1,23 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+void insert(int value);
+void print();
+void delete(int del);
 struct abc
 {
 	int data;
 	struct abc *next;
 };
-
+struct abc *head = NULL;
 int main()
 {
-	struct abc *head;
-	Printf("Enter the choice\n1.Insert the value\n2.Print the value");
-	int i, value;
-	scanf("%d", &i);
-	printf("\nEnter value");
-	scanf("%d", &value);
-	if(i == 1)
-		insert(value);
-	if(i ==2)
-		print();
+	while(1)
+	{
+		printf("\nEnter the choice\n 1. Insert the value\n 2. Print the value\n 3. Delete value\n 4. EXIT \n");
+		int i, value, del;
+		scanf("%d", &i);
+		if(i == 1)
+		{
+			printf("\nEnter value ");
+			scanf("%d", &value);
+			insert(value);
+		}	
+		if(i ==2)
+			print();
+		if(i ==3)
+		{
+			printf("\nEnter value to delete ");
+			scanf("%d", &del);
+			delete(del);
+		}
+		if(i == 4)
+		{
+			printf("Gaand Marao bsdke");
+			break;
+		}
+	}
 	
 	return 0;
 }
@@ -26,10 +45,26 @@ void insert(int value)
 {
 	struct abc *ptr;
 	ptr = (struct abc*)malloc(sizeof(struct abc));
-	ptr.data = value;
+	ptr->data = value;
+	ptr->next = head;
+	head = ptr;
 }
 
 void print()
+{
+	struct abc *ptr = head;
+	printf("\n{ ");
+	if(ptr==NULL)
+		printf("Beta kuch daalo toh sahi");
+	while(ptr!=NULL)
+	{
+		printf("%d ",ptr->data);
+		ptr = ptr->next;
+	}
+	printf(" }");
+}
+
+void delete(int del)
 {
 	
 }
