1872a2998886960e03651458c19685dc31af35e8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-06 15:42:12 +0530

    Update Stacklist.java

--- 04_Stack/Stacklist.java
+++ 04_Stack/Stacklist.java
@@ -1,1 +1,98 @@
+import java.util.Scanner;
+import static java.lang.System.*;
 
+class Node
+{
+	int data;
+	Node next;
+}
+
+class Stack
+{
+	Node top;
+	// Constructor
+	public Stack()
+	{
+		this.top = null;
+	}
+	void push(int data)
+	{
+		Node obj = new Node();
+		if(obj == null)
+		{
+			out.println("Heap Overflow");
+			return;
+		}
+		else
+		{
+			out.println("Inserting " + data);
+			obj.data = data;
+			obj.next = top;
+			top = obj;
+		}
+	}
+	public void peek()
+	{
+		if(top == null)
+		{
+			out.println("Stack Underflow");
+			return;
+		}
+		else
+		{
+			out.println(top.data);
+		}
+	}
+	public void pop()
+	{
+		if(top == null)
+		{
+			out.println("Stack Underflow");
+			return;
+		}
+		else
+		{
+			out.println("Removed ");
+			peek();
+			top = top.next;
+		}
+	}
+	void print()
+	{
+		out.println("Stack Elements");
+		if(top == null)
+		{
+			out.println("Stack Underflow");
+			return;
+		}
+		else
+		{
+			Node temp = top;
+			while(temp!=null)
+			{
+				out.println(temp.data);
+				temp = temp.next;
+			}
+		}
+	}
+}
+
+class Stacklist
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		Stack obj = new Stack();
+		obj.push(5);
+		obj.push(4);
+		obj.push(3);
+		obj.push(2);
+		obj.push(1);
+		obj.print();
+		out.println("The TOS is ");
+		obj.peek();
+		obj.pop();
+		obj.print();
+		
+	}
+}
