2c307ba72b5247ee7a770d119756a562a84d3a8e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-07 15:15:02 +0530

    Update Linkedlist1.java

--- 03_LinkedList/Linkedlist1.java
+++ 03_LinkedList/Linkedlist1.java
@@ -6,61 +6,128 @@ class Node
 	int data;
 	Node prev;
 	Node next;
+	
+	public Node(int data)
+	{
+		this.data = data;
+	}
 }
 
 class Linked
 {
+	Node head = null;
+	
+	
 	void insert(int val)
 	{
-		Node n = new Node();
-		n.data = val;
+		Node n = new Node(val);
 		n.next = null;
-		n.prev = null;
+		
 		if(head == null)
 		{
 			head = n;
-			out.println("Value Inserted");
+			head.prev = null;
+			System.out.println("Value Inserted");
 		}
 		else
 		{
-			while()	
+			Node temp = head;
+			while(temp.next != null)
+			{
+				temp = temp.next;
+			}
+			temp.next = n;
+			n.prev = temp;
+				
 		}
 		
 	}
 	
-	void search(int val)
+	void show()
 	{
-		
+		if(head == null)
+		{
+			System.out.println("----Empty List---");
+		}
+		else
+		{
+			Node n = head;
+			System.out.println();
+			while(n!=null)
+			{
+				System.out.print(n.data + ", ");
+				n = n.next;
+			}
+			System.out.print(".");
+			System.out.println();
+		}
 	}
 	
-	void del(int val)
+	void del(int ind)
 	{
 		
+		if(head == null)
+		{
+			out.println("Empty list");
+			return;
+		}
+		if(ind == 0)
+		{
+			head = head.next;
+			head.prev = null;
+			out.println("Index value deleted");
+		}
+		else
+		{
+			Node n = head;
+			for(int i = 0; i < ind-1; i++)
+			{
+				n = n.next;
+			}
+			Node temp = n.next;
+			Node temp1 = n.prev;
+			n.next = temp.next;
+			n.prev = temp1;
+			out.println("Index Deleted");
+		}
 	}
 }
 
 class DoubleLinked
 {
-	Scanner sc = new Scanner(System.in);
-	Linked obj = new Linked();
-	Node head = null; 	
-	out.println("---------Hello Welcome to Double Linked List----------");
-	int n;
-	while(true)
+	public static void main(String args[])
 	{
-		out.println("Enter your choice o =f Operation:\n1. Insert\n2. Print\n3. Delete\n4. Exit");
-		n = sc.nextInt();
-		if(n == 1)
+		Scanner sc = new Scanner(System.in);
+		Linked obj = new Linked();
+		 	
+		System.out.println("--Double Linked List--");
+		int n;
+		while(true)
 		{
-			out.pritnln("How many values to be entered");
-			int a = sc.nextInt();
-			for(int i = 0; i < a; i++)
+			out.println("Enter your choice of Operation:\n1. Insert\n2. Print\n3. Delete\n4. Exit");
+			n = sc.nextInt();
+			if(n == 1)
 			{
-				out.println("Enter value");
+				System.out.println("Enter value");
 				int val = sc.nextInt();
-				obj.insert(int val);
+				obj.insert(val);
+			}
+			else if(n == 2)
+			{
+				obj.show();
+			}
+			else if(n == 3)
+			{
+				out.println("Enter index of the value to delete");
+				int ind = sc.nextInt();
+				obj.del(ind);
+			}
+			else
+			{
+				out.println("EXIT");
+				sc.close();
+				break;
 			}
 		}
 	}
-	
-}
+}	
