0f3eec6d36ff58760eb843f5a2a050dc38bb1be2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-10 16:11:20 +0530

    Update Binarytree.java

--- 06_Tree/Binarytree.java
+++ 06_Tree/Binarytree.java
@@ -1,1 +1,29 @@
+class Node
+{
+	int data;
+	Node left, right;
+	
+	public Node(int data)
+	{
+		this.data = data;
+		left = right = null;
+	}
+}
 
+class BST
+{
+	Node root;
+	public BST(int data)
+	{
+		root = new Node(data);
+	}
+	public BST()
+	{
+		root = null;
+	}
+	BST obj = new BST();
+	obj.root = new Node(40);
+	obj.root.left = new Node(20);
+	obj.root.right = new Node(60);
+	
+} 
