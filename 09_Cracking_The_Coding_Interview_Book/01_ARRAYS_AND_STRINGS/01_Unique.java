8d4cfd8464b08281eb097f1a5491fd832c8f26f0
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-14 20:52:11 +0530

    Update 01_Unique.java

--- 09_Cracking_The_Coding_Interview_Book/01_ARRAYS_AND_STRINGS/01_Unique.java
+++ 09_Cracking_The_Coding_Interview_Book/01_ARRAYS_AND_STRINGS/01_Unique.java
@@ -1,1 +1,33 @@
-
+import java.util.Scanner;
+public class Unique
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string..");
+		String s = sc.nextLine();
+		boolean b = isUniqueChars(s);
+		if(b)
+			System.out.println("Entered String has unique Characters");
+		else
+			System.out.println("Entered String doesn't have unique Characters");
+	}
+	
+	public static boolean isUniqueChars(String str) 
+	{
+		if (str.length() > 128) 
+		{
+			return false;
+		}
+		boolean[] char_set = new boolean[128];
+		for (int i = 0; i < str.length()-1; i++)
+		{
+			int val = str.charAt(i);
+			if (char_set[val]) 
+				return false;
+			char_set[val] = true;
+		}
+		return true;
+	}
+	
+}
