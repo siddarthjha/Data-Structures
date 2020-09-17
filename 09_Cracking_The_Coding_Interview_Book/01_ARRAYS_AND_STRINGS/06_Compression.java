ad7733e88178efdf759a5ff6185b77b59416297a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-17 11:56:20 +0530

    Update 06_Compression.java

--- 09_Cracking_The_Coding_Interview_Book/01_ARRAYS_AND_STRINGS/06_Compression.java
+++ 09_Cracking_The_Coding_Interview_Book/01_ARRAYS_AND_STRINGS/06_Compression.java
@@ -1,1 +1,29 @@
-
+import java.util.Scanner;
+public class Compression
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		Compression obj = new Compression();
+		System.out.println(obj.compress(str));
+	}
+	
+	public String compress(String s)
+	{
+		String shortstring = "";
+		int count = 0;
+		
+		for(int i = 0; i < s.length(); i++)
+		{
+			count++;
+			if(i + 1 >= s.length() || s.charAt(i) != s.charAt(i+1))
+			{
+				shortstring += "" + s.charAt(i) + count;
+				count = 0; 
+			}
+		}
+		
+		return shortstring.length() < s.length() ? shortstring : s;
+	}
+}
