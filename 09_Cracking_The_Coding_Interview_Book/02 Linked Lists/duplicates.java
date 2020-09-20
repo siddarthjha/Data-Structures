2361a8d52f196c187d984387ba0768f41f19dc42
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-20 12:15:11 +0530

    Update duplicates.java

--- 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/duplicates.java
+++ 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/duplicates.java
@@ -1,1 +1,15 @@
-
+public void dups()
+{
+  Node curr = head;
+  while(curr!= null)
+  {
+    Node runn = curr;
+    while(runn!= null)
+    {
+      if(runn.next.data == curr.data) // If the data is same then removed else iterate next value
+          runn.next = runn.next.next;
+      else
+        runn = runn.next;
+    }
+    curr = curr.next;
+  }
