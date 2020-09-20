3abbeece0aa47bbd4a2a53cacd16b688240ad5e6
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-20 12:21:32 +0530

    Update 03_del_middle_node.java

--- 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/03_del_middle_node.java
+++ 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/03_del_middle_node.java
@@ -1,1 +1,11 @@
-
+// The "n" points to that particular node and we need to delete it.
+void middlenode(Node n)
+{   
+    if(n == null || n.next == null) // Then it is not middle node
+            return;
+    
+    Node next = n.next;
+    n.data = next.data;
+    n.next = next.next;
+}
+  
