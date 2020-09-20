7063faf154f2ee881a162634f9ddca6dc0260205
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-20 12:11:15 +0530

    Update kth_to_last.java

--- 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/kth_to_last.java
+++ 09_Cracking_The_Coding_Interview_Book/02 Linked Lists/kth_to_last.java
@@ -1,1 +1,20 @@
-
+	//K-th to last print
+	public void kth(int ind)
+	{
+		Node n = head;
+		for(int i = 0; i < ind; i++)
+		{
+			if(n == null)
+			{
+				out.println("bye");
+				break;
+			}
+			n = n.next;
+		}
+		while(n!= null)
+		{
+			System.out.print(n.data + ", ");
+			n = n.next;
+		}
+		out.println();
+	}
