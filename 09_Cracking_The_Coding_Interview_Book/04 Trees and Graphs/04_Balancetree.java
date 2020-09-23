31d58bb111443cef02f5c80358b363121b772105
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-23 19:18:33 +0530

    Update 04_Balancetree.java

--- 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/04_Balancetree.java
+++ 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/04_Balancetree.java
@@ -1,1 +1,23 @@
-
+boolean s = true; // global variable
+	int height(Node node)
+	{
+		if(node == null)
+			return 0;
+		else
+		{
+			int lh = height(node.left);
+			System.out.println("Lh = " + lh);
+			int rh = height(node.right);
+			System.out.println("Rh = " + rh);
+			if(!(Math.abs(lh-rh) <= 1))
+			{
+				s = false;
+				return 0;
+			}
+			if(lh > rh)
+				return (lh + 1);
+			else
+				return (rh + 1);
+		}
+		
+	}
