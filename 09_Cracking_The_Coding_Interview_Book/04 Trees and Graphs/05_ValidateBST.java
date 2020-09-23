fcb3e566444eb34de2f13a33ca189f5aa5ed4990
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-23 19:22:50 +0530

    Update 05_ValidateBST.java

--- 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/05_ValidateBST.java
+++ 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/05_ValidateBST.java
@@ -1,1 +1,19 @@
-
+// INORDER TRAVERSAL WILL GIVE SORTED VALUES IF NOT SORTED THEN NOT BST
+void inorder(Node node)
+	{
+		if(root == null)
+		{
+			System.out.println("Empty tree");
+			return;
+		}
+		else
+		{
+			if(node != null)
+			{
+				inorder(node.left);
+				System.out.print(node.data+ " ");
+				inorder(node.right);
+			}
+		}
+		//System.out.println();
+	}
