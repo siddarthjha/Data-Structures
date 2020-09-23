c89be0e0dd46d6f684edb4efd9c314ac858adb1f
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-23 22:00:56 +0530

    Update 10_Check_Subtree.java

--- 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/10_Check_Subtree.java
+++ 09_Cracking_The_Coding_Interview_Book/04 Trees and Graphs/10_Check_Subtree.java
@@ -1,1 +1,12 @@
-
+boolean areIdentical(Node root1, Node root2)  // Checking root2 tree is a subtree of root1
+    {   
+        if (root1 == null && root2 == null) 
+            return true; 
+   
+        if (root1 == null || root2 == null) 
+            return false; 
+   
+        /* Check if the data of both roots is same and data of left and right 
+           subtrees are also same */
+        return (root1.data == root2.data && areIdentical(root1.left, root2.left) && areIdentical(root1.right, root2.right)); 
+    }
