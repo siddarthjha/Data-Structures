62659ac81e94a2763429d3184b7f69ba8cb59f9c
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-23 18:34:24 +0530

    Update Tree_height.java

--- 06_Tree/Tree_height.java
+++ 06_Tree/Tree_height.java
@@ -1,1 +1,16 @@
+int height(Node node)
+{
+  if(root == null)
+      return 0;
+  else
+  {
+    int lh = height(node.left);
+    int rh = height(node.right);
+    
+    if(lh > rh)
+        return (lh + 1);
+    else
+      return (rh + 1);
+  }
+}
 
