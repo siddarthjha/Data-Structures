58575327a7cf3e079cb00b251115e46c11202e4f
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-24 19:05:11 +0530

    Update 07_Swap.c

--- 09_Cracking_The_Coding_Interview_Book/05 Bit Manipulation/07_Swap.c
+++ 09_Cracking_The_Coding_Interview_Book/05 Bit Manipulation/07_Swap.c
@@ -1,1 +1,21 @@
+ 
+#include <stdio.h> 
+
+// Function to swap odd and even bits
+unsigned int swapBits(unsigned int x) 
+{ 
+	unsigned int even_bits = x & 0xAAAAAAAA; // Even bits 
+	unsigned int odd_bits = x & 0x55555555; // Odd bits
+	even_bits >>= 1; // Right shift even bits 
+	odd_bits <<= 1; // Left shift odd bits 
+	return (even_bits | odd_bits); // Combine even and odd bits 
+} 
+
+ 
+int main() 
+{ 
+	unsigned int x = 23; // 00010111 
+	printf("%u ", swapBits(x)); 
+	return 0; 
+} 
 
