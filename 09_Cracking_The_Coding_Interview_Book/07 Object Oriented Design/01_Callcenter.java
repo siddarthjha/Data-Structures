e7f53f3ca580b96d3f0b7816af6acf4a1e47eda8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-26 18:30:07 +0530

    Update 01_Callcenter.java

--- 09_Cracking_The_Coding_Interview_Book/07 Object Oriented Design/01_Callcenter.java
+++ 09_Cracking_The_Coding_Interview_Book/07 Object Oriented Design/01_Callcenter.java
@@ -1,1 +1,69 @@
+class CallHandler
+{
+  final int levels = 3;
+  final int respondants = 10;
+  final int managers = 3;
+  final int directors = 2;
+  // List of employess level
+  List<List<Employee>> employelevels;
+  List<List<Call>> callqueue;
+  
+  public callhandler()
+  {
+    //...
+  }
+  
+  public Employee getHandler(Call call)
+  {
+    //...
+  }
+  
+  public void dispatchCall(Call call)
+  {
+    //...
+  }
+  
+  public boolean assignCall(Employee emp)
+  {
+    //...
+  }
+  
+}
+class Call
+{
+  private Rank rank;
+  public Call(caller c)
+  {
+    rank = Rank.responder
+      caller = c;
+  // Call represents a call from user and assigned to first minimum employeer
+}
+
+class Employee
+{
+  // This class is super class for Director Manager Respondent
+  public Employee(CallHandler h)
+  {
+    //...
+  }
+  public void callEscalteAndAssign()
+  {
+    //...
+  }
+}
+  
+class Director extends Employee
+{
+  //...
+}
+class Manager extends Employee
+{
+  //...
+}
+class Respondent extends Employee
+{
+  //...
+}
+
+    //
 
