2a2102424f03da28bade1356394eda9d841aa94e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-28 17:08:45 +0530

    Update Sort.java

--- 02_Arrays/Sort.java
+++ 02_Arrays/Sort.java
@@ -4,7 +4,7 @@ public class Sort
 	{
 		int arr[] = {1, 6, 7, 10, 2, 69, 40, 23, 58};
 		System.out.println("The array initially");
-		for(int i = 0; i < arr.length - 1; i++)
+		for(int i = 0; i < arr.length; i++)
 		{
 			System.out.print(arr[i] + " ");
 		}
@@ -12,11 +12,12 @@ public class Sort
 		Sort obj = new Sort();
 		obj.SelectionSort(arr);
 		obj.BubbleSort(arr);
+		obj.InsertionSort(arr);
 	}
 	
 	public void SelectionSort(int arr[])
 	{
-		for(int i = 0; i < arr.length -2; i++)
+		for(int i = 0; i < arr.length - 1; i++)
 		{
 			for(int j = i+1; j < arr.length; j++)
 			{
@@ -29,7 +30,7 @@ public class Sort
 			}
 		}
 		System.out.println("Selection Sort Result:");
-		for(int  i = 0; i < arr.length - 1; i++)
+		for(int  i = 0; i < arr.length; i++)
 		{
 			System.out.print(arr[i] + " ");
 		}
@@ -38,15 +39,44 @@ public class Sort
 	
 	public void BubbleSort(int arr[])
 	{
-		for(int i = 0; i <arr.length; i++)
+		for(int i = 0; i < arr.length; i++)
 		{
-			for(int j = ; j < arr.length; j++)
+			for(int j = 0; j < arr.length - i - 1; j++)
 			{
-				if(arr[i] > arr[j])
+				if(arr[j] > arr[j + 1])
 				{
-					
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 				}
 			}
 		}
+		System.out.println("Bubble Sort Sort Result:");
+		for(int  i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public void InsertionSort(int arr[])
+	{
+		for(int i = 1; i < arr.length; i++)
+		{
+			int val = arr[i];
+			int j = i-1;
+			while(j >= 0 && arr[j] > val)
+			{
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = val;
+		}
+		System.out.println("Insertion Sort Sort Result:");
+		for(int  i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 }
