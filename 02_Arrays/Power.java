da602a64b3cf3580529df92b1d868445264b4c1f
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-29 08:58:09 +0530

    Update Power.java

--- 02_Arrays/Power.java
+++ 02_Arrays/Power.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class Power
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number(Base) for power");
+		int n = sc.nextInt();
+		System.out.println("Enter the n-th power(Exponent)");
+		int x = sc.nextInt();
+		sc.close();
+		int temp = 1;
+		for(int i = 0; i < x; i++)
+		{
+			temp = temp * n;
+			System.out.print(temp + " ");
+		}
+		System.out.println("final = " + temp);
+	}
+}
