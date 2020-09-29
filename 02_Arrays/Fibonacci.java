e0ea70fbe3c03e60bdd06d6bfd17d4397882af87
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-29 09:35:17 +0530

    Update Fibonacci.java

--- 02_Arrays/Fibonacci.java
+++ 02_Arrays/Fibonacci.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;
+public class Fibonacci
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = sc.nextInt();
+		sc.close();
+		Fibonacci obj = new Fibonacci();
+		obj.fib(n);
+	}
+	
+	public void fib(int n)
+	{
+		int a = 0, b = 1;
+		System.out.print(a + " " + b);
+		for(int i = 3; i <= n; i++)
+		{
+			int c = a + b;
+			a = b; 
+			b = c;
+			System.out.print(" " + c);
+		}
+	}
+}
