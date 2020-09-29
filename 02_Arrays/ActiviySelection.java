088b3be7a5ed38a28d58bf28547c897d9dd09484
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-29 10:02:20 +0530

    Update ActiviySelection.java

--- 02_Arrays/ActiviySelection.java
+++ 02_Arrays/ActiviySelection.java
@@ -1,1 +1,26 @@
-
+public class ActiviySelection
+{
+	public static void main(String [] args)
+	{	
+		ActiviySelection obj = new ActiviySelection();
+		int s[] = {1, 3, 4, 5, 3, 7, 10, 8, 8, 12, 13};
+		int f[] = {4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15}; // Already sorted in ascending order
+		obj.schedule(s, f, s.length);
+	}
+	
+	public void schedule(int s[], int f[], int len)
+	{
+		int i, j;
+		i = 0;
+		System.out.println("The activities selected are :");
+		System.out.print(i + " ");
+		for(j = 1; j < len; j++)
+		{
+			if(s[j] >= f[i])
+			{
+				System.out.print(j + " ");
+				i = j;
+			}
+		}
+	}
+}
