7fac171e397c2807a358f906538d783e3c28a8d0
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-10 21:39:58 +0530

    Update BuySell_Stock.java

--- 02_Arrays/BuySell_Stock.java
+++ 02_Arrays/BuySell_Stock.java
@@ -1,1 +1,17 @@
-
+public class BuySell_Stock
+{
+	public static void main(String [] args)
+	{
+		int arr[] = {8, 5, 7, 1, 6, 4, 10, 2};
+		int min = Integer.MAX_VALUE;
+		int profit = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] < min)
+				min = arr[i];
+			else if(arr[i] - min > profit)
+				profit = arr[i] - min;
+		}
+		System.out.println("Profit = " + profit);
+	}
+}
