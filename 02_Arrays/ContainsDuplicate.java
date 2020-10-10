c86aafbb659bea76da650bbc84d93ac74c40aee1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-10 21:52:12 +0530

    Update ContainsDuplicate.java

--- 02_Arrays/ContainsDuplicate.java
+++ 02_Arrays/ContainsDuplicate.java
@@ -1,1 +1,14 @@
-
+import java.util.Arrays;
+public class ContainsDuplicate
+{
+	public static void main(String[] args)
+	{
+		int arr[] = {1, 2, 1, 3};
+		Arrays.sort(arr);
+		for(int i = 0; i < arr.length - 1; i++)
+		{
+			if(arr[i] == arr[i + 1])
+				System.out.println("Duplicate");
+		}
+	}
+}
