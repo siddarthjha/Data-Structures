cff5547d3a31d2667bb292e5fc078eab2dd56042
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-10 21:53:01 +0530

    Update ContainsDuplicate.java

--- 02_Arrays/ContainsDuplicate.java
+++ 02_Arrays/ContainsDuplicate.java
@@ -4,7 +4,7 @@ public class ContainsDuplicate
 	public static void main(String[] args)
 	{
 		int arr[] = {1, 2, 1, 3};
-		Arrays.sort(arr);
+		Arrays.sort(arr); // We can use any sort algorithm which gives runtime of O(n.logn)
 		for(int i = 0; i < arr.length - 1; i++)
 		{
 			if(arr[i] == arr[i + 1])
