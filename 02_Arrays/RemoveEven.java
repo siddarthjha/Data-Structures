d43b9253f6978f2b5e4f1a44b5d5195e18bfab2a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-17 19:30:22 +0530

    Update RemoveEven.java

--- 02_Arrays/RemoveEven.java
+++ 02_Arrays/RemoveEven.java
@@ -1,1 +1,17 @@
-
+public class RemoveEven
+{
+	public static void main(String[] args)
+	{
+		int arr[] = {1, 4, 7, 2, 6, 3, 9};
+		int c = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] % 2 == 1)
+				arr[c++] = arr[i];
+		}
+		for(int i = 0; i < c; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
