873beade22b2196844e7ac4beea1d7793cbfebbd
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-18 20:58:13 +0530

    Update SecondLargest.java

--- 02_Arrays/SecondLargest.java
+++ 02_Arrays/SecondLargest.java
@@ -1,1 +1,18 @@
-
+public class SecondLargest
+{
+	public static void main(String[] args)
+	{
+		int arr[] = {1, 3, 7, 2, 6, 4, 9};
+		int lar = arr[0], i = 0, s_lar = 0;
+		for(i = 1; i < arr.length; i++)
+		{
+			lar = Math.max(lar, arr[i]);
+		}
+		for(i = 0; i < arr.length; i++)
+		{
+			if(arr[i] != lar)
+				s_lar = Math.max(s_lar, arr[i]);
+		}
+		System.out.println("Second Largest element is " + s_lar);
+	}
+}
