931094e259f8925ac8680d969741ffce14ddbfda
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-18 18:41:49 +0530

    Update Min.java

--- 02_Arrays/Min.java
+++ 02_Arrays/Min.java
@@ -1,1 +1,14 @@
-
+public class Min
+{
+	public static void main(String[] args)
+	{
+		int arr[] = {1, 4, 7, 2, 6, 3, 9};
+		int max = 0, res = arr[0], min = 0;
+		for(int i = 1; i < arr.length; i++)
+		{
+			max = Math.max(res, arr[i]);
+			min = Math.min(res, arr[i]);
+		}
+		System.out.println("The minimum element of array = " + min + " max = " + max);
+	}
+}
